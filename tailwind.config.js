f8b80f21bb29a15dd717ed7dcab400c69b8c08f1
lalalalaLars <91459426+lalalalaLars@users.noreply.github.com>
2024-02-24 11:12:42 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBackgroundColor: "#0D1117",
+        columnBackgroundColor: "#161C22",
+      },
+    },
+  },
+  plugins: [],
+};
